2e19f5331e92b7cf3f5e4ca9ee8b5150941b03bf
Natalia Kazakova (DevExpress) <natalia.kazakova@devexpress.com>
2021-04-12 19:00:56 +0300

    init repo

---
+++ CS/BlazorDashboardApp/Startup.cs
@@ -0,0 +1,67 @@
+using BlazorDashboardApp.Data;
+using DevExpress.DashboardAspNetCore;
+using DevExpress.DashboardCommon;
+using DevExpress.DashboardWeb;
+using DevExpress.DataAccess.Json;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace BlazorDashboardApp {
+    public class Startup {
+        public Startup(IConfiguration configuration, IWebHostEnvironment hostingEnvironment) {
+            Configuration = configuration;
+            FileProvider = hostingEnvironment.ContentRootFileProvider;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IFileProvider FileProvider { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+            services.AddMvc()
+                    .AddDefaultDashboardController(configurator => {
+                    // Register Dashboard Storage
+                    configurator.SetDashboardStorage(new DashboardFileStorage(FileProvider.GetFileInfo("App_Data/Dashboards").PhysicalPath));
+                    // Create a sample JSON data source
+                    DataSourceInMemoryStorage dataSourceStorage = new DataSourceInMemoryStorage();
+                        DashboardJsonDataSource jsonDataSourceUrl = new DashboardJsonDataSource("JSON Data Source (URL)");
+                        jsonDataSourceUrl.JsonSource = new UriJsonSource(
+                                new Uri("https://raw.githubusercontent.com/DevExpress-Examples/DataSources/master/JSON/customers.json"));
+                        jsonDataSourceUrl.RootElement = "Customers";
+                        dataSourceStorage.RegisterDataSource("jsonDataSourceUrl", jsonDataSourceUrl.SaveToXml());
+                        configurator.SetDataSourceStorage(dataSourceStorage);
+                    });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            } else {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => {
+                EndpointRouteBuilderExtension.MapDashboardRoute(endpoints, "api/dashboard");
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Get Started - Dashboard Component in Blazor Server Application
+
+This project demonstrates how you can incorporate a DevExpress Dashboard component into a [Blazor Server](https://docs.microsoft.com/en-us/aspnet/core/blazor/hosting-models?view=aspnetcore-5.0#blazor-server) application. Use it as a template when you need to create a similar web application.
+
+The backend is an ASP.NET Core application that process requests from the client. UI updates, event handling, and JavaScript calls are handled over a SignalR connection. 
+
+## Documentation
+
+- [Create a Blazor Server Dashboard Application](https://docs.devexpress.com/Dashboard/403029?v=21.1)
+
+## More Examples
+
+- [Get Started - Dashboard Component in Blazor WebAssembly Application](https://github.com/natakazakova/dashboard-blazor-webassembly-app)
